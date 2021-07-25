c328283b45be54a12eebb09cabb6a141beeacc13
wkyoung <wwoj0205@gmail.com>
2021-07-25 09:56:30 +0900

    자주쓰는 스와이퍼 추가옵션 설정

--- swiper_slide/js/main.js
+++ swiper_slide/js/main.js
@@ -1,1 +1,9 @@
-const swiper = new Swiper('#wrap');
+const swiper = new Swiper('#wrap', {
+    slidesPerView: "3", //한 화면에 보일 슬라이드 갯수
+    loop: true, //슬라이드를 무한 순환
+    spaceBetween: 50, //슬라이드 사이 간격
+    centeredSlides: true, //슬라이드 가운데 정렬
+    speed: 1000, //슬라이드 이동 속도
+    grabCursor: true, //커서 모양 변경
+    
+});
