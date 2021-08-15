140aeadb471653c9d4016fe969fb0989cee86ead
wonkyoung-kim <wwoj0205@gmail.com>
2021-08-15 11:43:56 +0900

    버튼활성화 반복문 처리

--- scroll_test/myScroll.mjs
+++ scroll_test/myScroll.mjs
@@ -4,10 +4,11 @@ const sec = document.querySelectorAll('section');
 const btn = document.querySelectorAll('ul li');
 let sec_arr = [];
 
+//section의 갯수만큼 반복을 돌면서 배열에 세로 위치값 저장
 sec.forEach(el=>{
     sec_arr.push(el.offsetTop);
 });
-
+//버튼의 갯수만큼 반복을 돌면서 클릭이벤트 연결하고 animate함수 호출
 btn.forEach((el, index)=>{
     el.addEventListener('click', e=>{
         animate(window, {
@@ -17,3 +18,21 @@ btn.forEach((el, index)=>{
         });
     })
 })
+
+//스크롤 이벤트 발생시 스크롤 거리값과 각 섹션의 위치값을 비교해서 버튼활성화
+window.addEventListener('scroll', ()=>{
+    let scroll = window.scrollY || window.pageYOffset;
+    
+    sec_arr.forEach((el, index)=>{
+        if(scroll >= el){
+            activation(btn, index);
+        }
+    })
+});
+
+function activation(arr, index){
+    for(let el of arr){
+        el.classList.remove('on');
+    }
+    arr[index].classList.add('on');
+}
