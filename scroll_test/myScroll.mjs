20fe12e74aa2b3f379f09085170b0dd4e6ce6b22
wonkyoung-kim <wwoj0205@gmail.com>
2021-08-15 11:24:50 +0900

    스크롤버튼 클릭시 해당section으로 이동함수 적용

--- scroll_test/myScroll.mjs
+++ scroll_test/myScroll.mjs
@@ -1,1 +1,19 @@
 import {animate} from "./animate.mjs";
+
+const sec = document.querySelectorAll('section');
+const btn = document.querySelectorAll('ul li');
+let sec_arr = [];
+
+sec.forEach(el=>{
+    sec_arr.push(el.offsetTop);
+});
+
+btn.forEach((el, index)=>{
+    el.addEventListener('click', e=>{
+        animate(window, {
+            prop: 'scroll',
+            value: sec_arr[index],
+            duration: 500
+        });
+    })
+})
